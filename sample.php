c6a635dbdca6072a5eb868c0ef7f47742b6de6c1
Tarako0329 <green.green.midori@gmail.com>
2022-10-19 15:30:57 +0900

    ver2.00.0-0070:デザインの微調整

--- sample.php
+++ sample.php
@@ -1,27 +1,4 @@
 <?php
-require "php_header.php";
+//require "php_header.php";
+setCookie("test_cookie", "tesetest", time()+60*60*24*7, "/", "", TRUE, TRUE);
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<title>GPS の値を得る</title>
-</head>
- 
-<body>
-<?php
-    echo return_num_disp("1000000000")."<br>";
-    echo return_num_disp("10000000000")."<br>";
-    echo return_num_disp("100000000000")."<br>";
-    echo return_num_disp("1")."<br>";
-    echo return_num_disp("10")."<br>";
-    echo return_num_disp("100")."<br>";
-    echo return_num_disp("1000")."<br>";
-    echo return_num_disp("10,00")."<br>";
-    echo return_num_disp("1a000")."<br>";
-?> 
-<script>
-</script>
-</body>
-</html>
