1bee95a02e54fa6333858abc1661a44dde4bda1c
Tarako0329 <86461908+Tarako0329@users.noreply.github.com>
2024-06-11 11:35:18 +0900

    ver3.05.2-000:軽微な改良

--- php_header.php
+++ php_header.php
@@ -5,7 +5,7 @@ date_default_timezone_set('Asia/Tokyo');
 require "./vendor/autoload.php";
 require_once "functions.php";
 //本番はリリースした日を指定
-$time="20240607-03";
+$time="20240611-03";
 
 //.envの取得
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
