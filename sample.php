befc45921325d7d34c4b4e3b3f762f1881f7384e
Tarako0329 <86461908+Tarako0329@users.noreply.github.com>
2024-06-12 15:13:09 +0900

    ver3.07.0-002:住所変換処理

--- sample.php
+++ sample.php
@@ -1,7 +1,26 @@
 <?php
-$url = 'https://mreversegeocoder.gsi.go.jp/reverse-geocoder/LonLatToAddress?lat=43.0686718333333&lon=141.351173694444';
+    require "php_header.php";
 
-print_r(get_headers($url));
+    $sqlstr = "select * from UriageData_GioWeather order by ins_datetime desc";
+    $stmt = $pdo_h->prepare($sqlstr);
+    
+    $stmt->execute();
+    $result = $stmt->fetchall(PDO::FETCH_ASSOC);
 
-print_r(get_headers($url, true));
+
+    foreach($result as $row){
+        $url = 'https://mreversegeocoder.gsi.go.jp/reverse-geocoder/LonLatToAddress?lat='.$row["lat"].'&lon='.$row["lon"];
+        $RESULT = json_decode(file_get_contents($url), true);
+        print_r($RESULT["results"]["muniCd"]);
+        print_r($RESULT["results"]["lv01Nm"]);
+        
+        $stmt = $pdo_h->prepare("update UriageData_GioWeather set MUNI = '".$RESULT["results"]["muniCd"]."', address = '".$RESULT["results"]["lv01Nm"]."' where uid=".$row["uid"]." and UriNo =".$row["UriNo"]);
+        $stmt->execute();
+        //print_r(json_encode(file_get_contents($url), JSON_UNESCAPED_UNICODE));
+        //break;
+        usleep(500000);
+    }
+    
+
+
 ?>
