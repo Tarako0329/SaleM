c65d788103fb997534b4e15de96df3a4b8efb6f6
Tarako0329 <86461908+Tarako0329@users.noreply.github.com>
2024-07-12 18:45:43 +0900

    ver3.09.0-000:無料期間終了間近処理追加

--- menu.php
+++ menu.php
@@ -87,7 +87,7 @@ start(ajax関数名(固定値),ツアー名称(DBに登録する名称),ステ�
             if((strtotime($row[0]["yuukoukigen"]) - strtotime(date("Y-m-d")))/ (60 * 60 * 24) <= 7){
                 $msg = "<p class='mb-1 fs-3' style='color:red;'>残り ".(strtotime($row[0]["yuukoukigen"]) - strtotime(date("Y-m-d")))/ (60 * 60 * 24)."日 で無料期間が終了します。</p>";
                 $msg.="<p class='mb-1 fs-3'>引続きのご利用は<a href='".rot13decrypt2(PAY_CONTRACT_URL)."?system=".$title."&sysurl=".$root_url."&dirpath=".$dir_path."'>本契約</a>をお願いいたします。</p>";
-                $msg.="<p class='mb-1 fs-3'>なお、無料期間終了前のご契約でも無料期間後に本契約に切り替わります。</p>";
+                $msg.="<p class='mb-1 fs-3'>ご契約完了後、無料期間終了後から本契約に切り替わります。</p>";
             }
 
 
