e91ba3d5b040211e27fbaa57afd07dab0a2b4b59
Tarako0329 <86461908+Tarako0329@users.noreply.github.com>
2024-07-24 18:41:03 +0900

    ver3.11.0-000:freeeの設定追加

--- output_menu.php
+++ output_menu.php
@@ -9,10 +9,18 @@ if(!empty($_POST)){
     $ymto = $_POST["ymto"];
     $list = $_POST["list"];
     
-    $sqlstr="select Uridate,sum(UriageKin+zei) as zeikomi,CONCAT('売上No:',UriageNo) as 売上No,case when zeiKBN='1001' then '軽減税率8%' else '' end as 税率";
-    $sqlstr=$sqlstr." from UriageData where uid=? and Uridate between ? and ?";
-    $sqlstr=$sqlstr." group by Uridate,CONCAT('売上No:',UriageNo),case when zeiKBN='1001' then '軽減税率8%' else '' end";
-    $sqlstr=$sqlstr." order by Uridate,売上No";
+    if($_POST["type"]==="yayoi"){
+        $sqlstr="select Uridate,sum(UriageKin+zei) as zeikomi,CONCAT('売上No:',UriageNo) as 売上No,case when zeiKBN='1001' then '軽減税率8%' else '' end as 税率";
+        $sqlstr=$sqlstr." from UriageData where uid=? and Uridate between ? and ?";
+        $sqlstr=$sqlstr." group by Uridate,CONCAT('売上No:',UriageNo),case when zeiKBN='1001' then '軽減税率8%' else '' end";
+        $sqlstr=$sqlstr." order by Uridate,売上No";
+    }elseif($_POST["type"]==="freee"){
+        $sqlstr="select '収入' as 収支,CURDATE()+0,Uridate,'売上高' as 科目,sum(UriageKin) as 売上,'外税',sum(zei) as 税額,CONCAT('売上No:',UriageNo) as 備考,'事業主貸' as 決済口座";
+        $sqlstr=$sqlstr." from UriageData where uid=? and Uridate between ? and ?";
+        $sqlstr=$sqlstr." group by CURDATE()+0,Uridate,CONCAT('売上No:',UriageNo)";
+        $sqlstr=$sqlstr." order by Uridate,備考";
+
+    }
 
     $stmt = $pdo_h->prepare($sqlstr);
     $stmt->bindValue(1, $_SESSION["user_id"], PDO::PARAM_INT);
@@ -46,18 +54,6 @@ if(!empty($_POST)){
 
 <header class="header-color common_header">
     <div class="yagou title"><a href="menu.php"><?php echo $title;?></a></div></a></div>
-    <?php
-    //if($logoff==false){
-    ?>
-        <!--<div class="yagou title"><a href="menu.php"><?php //echo $title;?></a></div></a></div>
-        <span style="font-size:1.5rem;"><a href="menu.php?action=logout"><i class="fa-solid fa-right-from-bracket"></i></a></span>-->
-    <?php
-    //}else{
-    ?>
-        <!--<div class="yagou title"><a href="index.php"><?php //echo $title;?></a></div></a></div>-->
-    <?php
-    //}
-    ?>
 </header>
 
 <body class='common_body'>
@@ -70,12 +66,12 @@ if(!empty($_POST)){
         <input class='form-control' style="font-size:1.5rem;max-width:200px;" type='date' id='ymto' name='ymto' value='<?php echo $ymto; ?>'>
         <br>
         <label for='type'>連携会計システムの選択</label>
-        <select class='form-control' style="font-size:1.5rem;padding:0;max-width:400px;" name='type' id='type'>
-            <option value='yayoi'>やよいの青色申告</option>
-            <option value='free'>フリー</option>
+        <select class='form-select' style="font-size:1.5rem;padding:0;max-width:400px;" name='type' id='type'>
+            <option value='yayoi'>やよいの青色申告 オンライン</option>
+            <option value='freee'>freee会計(確定申告)</option>
         </select>
         <br>
-        <input class='btn btn-primary' type='submit' value='CSV出力' style='width:200px;hight:100px;'>
+        <input class='btn btn-primary' type='submit' value='CSV出力' style='width:200px;height:100px;'>
     </form>
     </div>
     
