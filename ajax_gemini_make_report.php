4dbb023f0116cd4ee684165e1e581b30cc28f238
Tarako0329 <86461908+Tarako0329@users.noreply.github.com>
2025-06-30 11:14:48 +0900

    ver3.21.7-000:A.Iレポート機能

--- ajax_gemini_make_report.php
+++ ajax_gemini_make_report.php
@@ -19,10 +19,12 @@ if($rtn !== true){
 	
 	//売上明細データを取得
 	$sql="SELECT 
-		UriageNO as 売上番号
+		DATE_FORMAT(UriDate, '%Y') as 売上計上年
+		,DATE_FORMAT(UriDate, '%Y-%m') as 売上計上月
 		,UriDate as 売上計上日
 		,Event as 売上計上イベント名
 		,TokuisakiNM as イベント以外の売上先
+		,UriageNO as 売上番号
 		,ShouhinNM as 商品名
 		,su as 売上個数
 		,tanka as 売上単価
@@ -38,14 +40,17 @@ if($rtn !== true){
 		,temp as 売上時の気温
 		,feels_like as 売上時の体感温度
 		from UriageMeisai 
-		where uid=? and UriDate between ? and ?
+		where uid=:uid and UriDate between :from_d and :to_d
+		order by UriDate desc,CONCAT(Event,TokuisakiNM) desc,UriageNO,ShouhinNM
 	";
 	
 
 	//UriageMeisaiから売上計上日、イベント、得意先、商品を集計キーとした売上集計データを取得するSQL文
 	//気温は平均。天気は最大カウントを取る天気
 	$sql_sum = "SELECT
-		UriDate as 売上計上日
+		DATE_FORMAT(UriDate, '%Y') as 売上計上年
+		,DATE_FORMAT(UriDate, '%Y-%m') as 売上計上月
+		,UriDate as 売上計上日
 		,Event as 売上計上イベント名
 		,TokuisakiNM as イベント以外の売上先
 		,ShouhinNM as 商品名
@@ -63,29 +68,36 @@ if($rtn !== true){
 		,ROUND(avg(temp),1) as 売上時の平均気温
 		,ROUND(avg(feels_like),1) as 売上時の平均体感温度
 		from UriageMeisai U
-		where uid=? and UriDate between ? and ?
+		where uid=:uid and UriDate between :from_d and :to_d
 		group by UriDate,Event,TokuisakiNM,ShouhinNM,bunrui1,bunrui2,bunrui3,address
 		order by UriDate desc
 	";
 
+	
+	$params["uid"]=$_SESSION['user_id'];
 
 	if($_POST["data_range"]==="過去５年の売上データをもとに"){
-		$stmt = $pdo_h->prepare($sql_sum);
-		$stmt->bindValue(1, $_SESSION['user_id'], PDO::PARAM_INT);
-		$stmt->bindValue(2, date('Y-m-d', strtotime('-5 year')), PDO::PARAM_STR);
-		$stmt->bindValue(3, date('Y-m-d'), PDO::PARAM_STR);
+		$sqlstr = $sql_sum;
+		$params['from_d'] = date('Y-m-d', strtotime('-5 year'));
+		$params['to_d'] = date('Y-m-d');
 	}else if($_POST["data_range"]==="直近１２ヵ月の売上データをもとに"){
-		$stmt = $pdo_h->prepare($sql);
-		$stmt->bindValue(1, $_SESSION['user_id'], PDO::PARAM_INT);
-		$stmt->bindValue(2, date('Y-m-d', strtotime('-12 month')), PDO::PARAM_STR);
-		$stmt->bindValue(3, date('Y-m-d'), PDO::PARAM_STR);
+		$sqlstr = $sql;
+		$params['from_d'] = date('Y-m-d', strtotime('-12 month'));
+		$params['to_d'] = date('Y-m-d');
 	}else if($_POST["data_range"]==="今年の売上データをもとに"){
-		$stmt = $pdo_h->prepare($sql);
-		$stmt->bindValue(1, $_SESSION['user_id'], PDO::PARAM_INT);
-		$stmt->bindValue(2, date('Y-01-01'), PDO::PARAM_STR);
-		$stmt->bindValue(3, date('Y-12-31'), PDO::PARAM_STR);
+		$sqlstr = $sql;
+		$params['from_d'] = date('Y-01-01');
+		$params['to_d'] = date('Y-12-31');
 	}
-	
+	//$sqlstrから改行を削除
+	$sqlstr = str_replace(array("\r\n", "\r", "\n"), ' ', $sqlstr);
+
+	$stmt = $pdo_h->prepare($sqlstr);
+	$stmt->bindValue("uid", $params['uid'], PDO::PARAM_INT);
+	$stmt->bindValue("from_d", $params['from_d'], PDO::PARAM_STR);
+	$stmt->bindValue("to_d", $params['to_d'], PDO::PARAM_STR);
+	$sqllog .= rtn_sqllog($sqlstr,$params);
+
 	$stmt->execute();
 	$shouhin_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -95,21 +107,39 @@ if($rtn !== true){
 	if($_POST["save_setting"]==="true"){
 		//ajax_delins_business_info.phpに保存
 		//ビジネス情報とプロンプトを保存
-		$sqlstr="DELETE from analysis_ai_setting where uid=:uid";
-		$stmt = $pdo_h->prepare($sqlstr);
-		$stmt->bindValue("uid", $_SESSION['user_id'], PDO::PARAM_INT);
-		$stmt->execute();
+		try {
+			$pdo_h->beginTransaction();
 
-		$sqlstr="INSERT into analysis_ai_setting(uid,ai_role,data_range,your_ask,report_type) values(:uid,:ai_role,:data_range,:your_ask,:report_type)";
-		$stmt = $pdo_h->prepare($sqlstr);
-		$stmt->bindValue("uid", $_SESSION['user_id'], PDO::PARAM_INT);
-		$stmt->bindValue("ai_role", $_POST["ai_role"], PDO::PARAM_STR);
-		$stmt->bindValue("data_range", $_POST["data_range"], PDO::PARAM_STR);
-		$stmt->bindValue("your_ask", $_POST["your_ask"], PDO::PARAM_STR);
-		$stmt->bindValue("report_type", $_POST["report_type"], PDO::PARAM_STR);
-		$stmt->execute();
-
-		
+			//$params["uid"]=$_SESSION['user_id'];
+			$params["ai_role"]=$_POST['ai_role'];
+			$params["data_range"]=$_POST['data_range'];
+			$params["your_ask"]=TRIM($_POST['your_ask']);
+			$params["report_type"]=TRIM($_POST['report_type']);
+			
+			$sqlstr="DELETE from analysis_ai_setting where uid=:uid";
+			$stmt = $pdo_h->prepare($sqlstr);
+			$stmt->bindValue("uid", $params['uid'], PDO::PARAM_INT);
+			$sqllog .= rtn_sqllog($sqlstr,$params);
+			$stmt->execute();
+			$sqllog .= rtn_sqllog("--execute():正常終了",[]);
+	
+			$sqlstr="INSERT into analysis_ai_setting(uid,ai_role,data_range,your_ask,report_type) values(:uid,:ai_role,:data_range,:your_ask,:report_type)";
+			$stmt = $pdo_h->prepare($sqlstr);
+			$stmt->bindValue("uid", $params['uid'], PDO::PARAM_INT);
+			$stmt->bindValue("ai_role", $params["ai_role"], PDO::PARAM_STR);
+			$stmt->bindValue("data_range", $params["data_range"], PDO::PARAM_STR);
+			$stmt->bindValue("your_ask", $params["your_ask"], PDO::PARAM_STR);
+			$stmt->bindValue("report_type", $params["report_type"], PDO::PARAM_STR);
+			$sqllog .= rtn_sqllog($sqlstr,$params);
+			$stmt->execute();
+			$sqllog .= rtn_sqllog("--execute():正常終了",[]);
+			$pdo_h->commit();
+			$sqllog .= rtn_sqllog("commit",[]);
+			sqllogger($sqllog,0);
+		} catch (Exception $e) {
+			$pdo_h->rollBack();
+			log_writer2(basename(__FILE__)." [Exception \$e] =>",$e,"lv0");
+		}
 	}
 	
 	if(EXEC_MODE<>"Local"){
